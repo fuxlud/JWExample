7e69d8be43a09ffe1fe73b99e56c472cac5b9cfa
Luda Fux <fuxlud@gmail.com>
2019-08-05 12:53:04 +0300

    Initial Commit

---
+++ JWExample/ViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+final class ViewController: UICollectionViewController {
+  // MARK: - Properties
+  private let reuseIdentifier = "Cell"
+  private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+  private let itemsPerRow: CGFloat = 3
+}
+
+// MARK: - UICollectionViewDataSource
+extension ViewController {
+
+  override func collectionView(_ collectionView: UICollectionView,
+                               numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                  for: indexPath) as! Cell
+    cell.backgroundColor = .blue
+
+    return cell
+  }
+}
+
+// MARK: - Collection View Flow Layout Delegate
+extension ViewController : UICollectionViewDelegateFlowLayout {
+
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+    let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+    let availableWidth = view.frame.width - paddingSpace
+    let widthPerItem = availableWidth / itemsPerRow
+    
+    return CGSize(width: widthPerItem, height: widthPerItem)
+  }
+
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      insetForSectionAt section: Int) -> UIEdgeInsets {
+    return sectionInsets
+  }
+
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return sectionInsets.left
+  }
+}
---
+++ Pods/Target Support Files/Pods-JWExample/Pods-JWExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JWExample : NSObject
+@end
+@implementation PodsDummy_Pods_JWExample
+@end
